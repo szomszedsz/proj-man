0c4558109dbb3a0cc73e739f60d904ac686f7299
szomszedsz <szomszed.szilard@gmail.com>
2022-02-11 13:50:34 +0100

    add route groupign

--- routes.php
+++ routes.php
@@ -4,18 +4,17 @@ use Welover\Controllers\Projects;
 use Welover\Controllers\ProjectsController;
 
 SimpleRouter::get('/', [ProjectsController::class, 'index']);
-// SimpleRouter::get('/project/edit/{id}', [ProjectsController::class, 'edit',function($id){}]);
-// SimpleRouter::get('/project/create', [ProjectsController::class, 'create']);
-// SimpleRouter::post('/project', [ProjectsController::class, 'store']);
-// SimpleRouter::put('/project', [ProjectsController::class, 'update']);
-
 
 SimpleRouter::group(['prefix' => '/project'], function () {
   SimpleRouter::get('/edit/{id}', [ProjectsController::class, 'edit',function($id){}]);
-SimpleRouter::get('/create', [ProjectsController::class, 'create']);
-SimpleRouter::post('', [ProjectsController::class, 'store']);
-SimpleRouter::put('', [ProjectsController::class, 'update']);
-
+  SimpleRouter::get('/create', [ProjectsController::class, 'create']);
+  SimpleRouter::post('', [ProjectsController::class, 'store']);
+  SimpleRouter::put('', [ProjectsController::class, 'update']);
 });
 
 SimpleRouter::delete('/api/project/delete/{id}', [ProjectsController::class, 'delete',function($id){}]);
+  SimpleRouter::group(['prefix' => '/api'], function () {
+    SimpleRouter::group(['prefix' => '/project'], function () {
+    SimpleRouter::delete('/delete/{id}', [ProjectsController::class, 'delete',function($id){}]);
+  });
+});
